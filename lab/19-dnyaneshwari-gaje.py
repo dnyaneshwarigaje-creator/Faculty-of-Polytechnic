fbbdda26d11be586c1dcb90875ab89bf6415abf1
dnyaneshwarigaje-creator <dnyaneshwarigaje@gmail.com>
2026-06-03 19:48:39 +0530

    Update 19-dnyaneshwari-gaje.py

--- lab/19-dnyaneshwari-gaje.py
+++ lab/19-dnyaneshwari-gaje.py
@@ -1,1 +1,35 @@
+t=0
+n=1
+while n<=10:
+  t+=n
+  n+=1
+  print(t)
+  #problem
+a=4
+b=9
+c=19
+for num in[a,b,c]:
+  if num %3==0:
+    print( num," is divisible by 3")
+  else:
+    print( num," is not divisible by 3")
+    #problem
+l= [1,2,3,4,5,6]
+l.pop(0)
+l.pop(2)
+l.pop(4)
+print(l)
+#problem
+l=[1,2,3,4,5,6]
+l.pop(0)
+l.pop(1)
+l.pop(2)
+print (l)
+#problem 
+l=[1,2,3,4,5,6]
+l.pop(4)
+l.pop(2)
+l.pop(0)
+print(1)
 
+    
