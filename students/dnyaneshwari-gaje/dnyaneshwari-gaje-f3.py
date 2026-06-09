a4abed164295a651ed5b3d04f0c2a2a73b66cda0
dnyaneshwarigaje-creator <dnyaneshwarigaje@gmail.com>
2026-06-09 11:50:09 +0530

    Update dnyaneshwari-gaje-f3.py

--- students/dnyaneshwari-gaje/dnyaneshwari-gaje-f3.py
+++ students/dnyaneshwari-gaje/dnyaneshwari-gaje-f3.py
@@ -1,1 +1,15 @@
-
+#problem
+num=10
+while True:
+  uinp=int (input ("1,2,3,4,5,6,7,8,9,10"))
+  if uinp !=num
+  print("incorrect")
+else:
+  print("correct")
+  #problem
+n=5
+student =[]
+for x in range(n):
+    name = input("enter student name:")
+    student.append(name)
+print (student)
